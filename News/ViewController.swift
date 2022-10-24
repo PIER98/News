0dfb59bf7feb17e1830bb2e06e091e806457208d
PIER98 <36538180+PIER98@users.noreply.github.com>
2022-10-24 10:56:44 +0200

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Pierpaolo Mariani on 24/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
