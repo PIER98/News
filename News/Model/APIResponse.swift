d8cfe14e45cb090c70fd53555d17f08face94d71
Pierpaolo Mariani <36538180+PIER98@users.noreply.github.com>
2022-11-12 15:22:36 +0100

    Create APIResponse.swift

---
+++ News/Model/APIResponse.swift
@@ -0,0 +1,29 @@
+//
+//  APIResponse.swift
+//  News
+//
+//  Created by Pierpaolo Mariani on 09/11/22.
+//
+
+import Foundation
+
+
+struct APIResponse  : Codable {
+    var articles : [Article]
+    var status : String
+}
+
+
+class Article : Codable {
+    var source : Source
+    var author : String?
+    var title : String?
+    var description : String?
+    var url : String?
+    var urlToImage : String?
+    var publishedAt : String?
+}
+
+struct Source : Codable {
+    var name : String
+}
