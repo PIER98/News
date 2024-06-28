8a4c0f073de5d79893819a02161ef164648ae8e4
Pierpaolo Mariani <36538180+PIER98@users.noreply.github.com>
2024-06-28 17:37:33 +0200

    CategoryCollectionViewCell refactor
    apply better code modularization

--- News/View/CategoryCollectionViewCell.swift
+++ News/View/CategoryCollectionViewCell.swift
@@ -8,15 +8,14 @@
 import UIKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var categoryLabel: UILabel!
-    
-    
-
-
+    @IBOutlet private weak var categoryLabel: UILabel!
     
     override func layoutSubviews() {
-        categoryLabel.layer.cornerRadius = 15
-        categoryLabel.backgroundColor =  #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+       setupViewLayer()
+       setupLabel()
+    }
+    
+    private func setupViewLayer() {
         self.layer.shadowColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         self.layer.shadowOffset = CGSize(width: 0, height: 0.5)
         self.layer.shadowOpacity = 0.1
@@ -24,5 +23,9 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         categoryLabel.layer.masksToBounds = true
     }
     
+    private func setupLabel() {
+        categoryLabel.layer.cornerRadius = 15
+        categoryLabel.backgroundColor =  #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+    }
 }
 
